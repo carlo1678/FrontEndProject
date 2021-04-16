d8e7139cd43bc2768913bb4cb04d9adeb4c84b39
carlo carbonell <carlo1678@hotmail.com>
2021-04-16 13:56:37 -0400

    commit final edits to project

--- project3.js
+++ project3.js
@@ -1,9 +1,14 @@
 const userInput = document.querySelector(".searchInput")
 const apiPull = async () => {
+    // loop through tags, set innerHTML as empty string
+    // const grabParagraphs = document.querySelectorAll("p")
+    // grabParagraphs.forEach((a) => {a.innerHTML = ""})
+    // if ()
     let response = await fetch(`https://api.nytimes.com/svc/books/v3/reviews.json?author=${userInput.value}&api-key=5BRNX9snHaZ283VWQa3Cui2EWgzQqo5G`) 
     let json = await response.json()
     let authorResponse = json.results
     let counter = 1
+    console.log(authorResponse)
     for (author of authorResponse) {
         if (counter > 9) break
         const grabCardBody = document.querySelector(`.card-body${counter}`)
@@ -14,11 +19,10 @@ const apiPull = async () => {
         grabCardTitle.innerText = author.book_title 
         const grabCardAuthor = document.querySelector(`.card-author${counter}`)
         grabCardAuthor.innerText = author.book_author
-        const img = document.createElement("img")
-        img.height = "125"
-        img.width = "300"
-        img.src = "https://i.pinimg.com/originals/62/7a/07/627a070b405403cda20e8a66d09c405a.gif"
-        grabCardBody.prepend(img)
+        const grabImg = document.querySelector(`.card-image${counter}`)
+        grabImg.height = "125"
+        grabImg.width = "300"
+        grabImg.src = "https://i.pinimg.com/originals/62/7a/07/627a070b405403cda20e8a66d09c405a.gif"
         // const grabCardLink = document.querySelector(`.card-link${counter}`)
         // const grabCard = document.querySelector(`card${counter}`)
         // grabCard.addEventListener("click", function(){
--- project4.js
+++ project4.js
@@ -1,5 +1,5 @@
 const userInput = document.querySelector(".searchInput")
-const apiPull = async () => {
+const apiPull = async () => { 
     let response = await fetch(`https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=${userInput.value}&api-key=5BRNX9snHaZ283VWQa3Cui2EWgzQqo5G`)
     let json = await response.json()
     let moviePull = json.results
@@ -7,15 +7,14 @@ const apiPull = async () => {
     for (movie of moviePull) {
         if (counter > 3) break
         const grabHeader = document.querySelector(`.card__header${counter}`)
-        grabHeader.innerText = movie.display_title
+        grabHeader.innerHTML = movie.display_title
         const grabText = document.querySelector(`.card__text${counter}`)
-        grabText.innerText = movie.summary_short
+        grabText.innerHTML = movie.summary_short
         const grabButton = document.querySelector(`.card__btn${counter}`)
         grabButton.addEventListener("click", function(){
             location.href = movie.link.url
         })
         counter += 1
-        console.log(moviePull[0])
         
     }
 
